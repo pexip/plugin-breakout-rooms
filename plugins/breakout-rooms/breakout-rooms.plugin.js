fd3e3261f3bcb748956723cd3f08a16ca0ea2115
Marcos Cereijo <marcoscereijo@gmail.com>
2022-04-18 17:27:36 +0200

    refactor: simplification of the code

--- plugins/breakout-rooms/breakout-rooms.plugin.js
+++ plugins/breakout-rooms/breakout-rooms.plugin.js
@@ -38,23 +38,16 @@
   }
 
   function showBreakRoomsDialog(participant) {
+    const breakoutRoomsNumber = 10;
     PEX.pluginAPI.openTemplateDialog({
       title: 'Select breakout room',
       body: `<div id="layout-info" style="flex-wrap: wrap; justify-content: center;">
               <!-- actors_overlay_text can be set to auto or off on each of the layouts.-->
               <div style="margin: 10px;">
               <span>Select the breakout room to transfer the user to:</span><br><br>
-              <select id="breakroom-select"> 
-                <option value="breakout-room-1">Breakout room 1</option> 
-                <option value="breakout-room-2">Breakout room 2</option>
-                <option value="breakout-room-3">Breakout room 3</option>
-                <option value="breakout-room-4">Breakout room 4</option>
-                <option value="breakout-room-5">Breakout room 5</option>
-                <option value="breakout-room-6">Breakout room 6</option>
-                <option value="breakout-room-7">Breakout room 7</option>
-                <option value="breakout-room-8">Breakout room 8</option>
-                <option value="breakout-room-9">Breakout room 9</option>
-              </select><br><br>
+              <select id="breakroom-select">` +
+              [...Array(breakoutRoomsNumber).keys()].map( (index) => `<option value="breakout-room-${index + 1}">Breakout room ${index + 1}</option> ` ) +
+              `</select><br><br>
               <button id="submit-plugin-breakout-rooms" class="dialog-button blue-action-button" style="border-radius: 10px; min-width: 0px;"> Move participant</button><br>
             </div>`
     })
